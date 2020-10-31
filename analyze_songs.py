5e74a56d21c027d569e8f2fa078e57f410704823
NinjadenMu <jaden.mu@gmail.com>
2020-10-31 12:27:49 -0400

    Update analyze_songs.py

--- analyze_songs.py
+++ analyze_songs.py
@@ -11,7 +11,7 @@ for i, track in enumerate(mid.tracks):
     for message in track:
         tracks[i].append(message)
 
-tracks[1] = tracks[1][8:]
+tracks[1] = tracks[1][7:]
 
 parsed_notes = []
 
@@ -19,7 +19,6 @@ i = 0
 
 for i in range(len(tracks[1]) - 1):
     if tracks[1][i].type == 'note_on':
-        print(i)
         parsed_notes.append([tracks[1][i].note])
         parsed_notes[int(i / 2)].append(tracks[1][i].velocity)
         parsed_notes[int(i / 2)].append(tracks[1][i + 1].time)
